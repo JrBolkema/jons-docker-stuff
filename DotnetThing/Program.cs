6603121ceddfe913240f9d6bbaf92d3529a6c995
Jon Bolkema <jrbolkema@gmail.com>
2022-05-10 16:40:48 -0400

    day 1

---
+++ DotnetThing/Program.cs
@@ -0,0 +1,17 @@
+﻿var fileName = "someText.txt";
+var current = 0;
+var path = Path.Join("data",fileName);
+
+if(File.Exists(path)){
+    var content = File.ReadAllText(path);
+    current = int.Parse(content);
+}
+Console.WriteLine( $"Current is {current}");
+
+while (true)
+{
+    Thread.Sleep(300);
+    current = ++current;
+    File.WriteAllText(path,current.ToString());
+    System.Console.WriteLine(  $"Wrote {current} at {DateTime.Now}");
+}
